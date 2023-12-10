3e1ab718941832903eaa16db761262ace47e7521
pius712 <aksfbsgnlfls@gmail.com>
2023-12-10 19:56:41 +0900

    first commit

---
+++ src/main/kotlin/com/example/springlogintutorial/SpringLoginTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springlogintutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringLoginTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringLoginTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springlogintutorial/SpringLoginTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springlogintutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringLoginTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
